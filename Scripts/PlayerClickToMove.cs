2b7e8a7682e72f5cb31d9d13f0fe0aedda2ff7eb
echolimauw <49353272+echolimauw@users.noreply.github.com>
2020-05-20 16:08:45 -0700

    Added basic player movement scripts

---
+++ Scripts/PlayerClickToMove.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class PlayerClickToMove : MonoBehaviour
+{
+    NavMeshAgent m_Agent;
+    RaycastHit m_HitInfo = new RaycastHit();
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !Input.GetKey(KeyCode.LeftShift))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray.origin, ray.direction, out m_HitInfo))
+                m_Agent.destination = m_HitInfo.point;
+        }
+    }
+}
