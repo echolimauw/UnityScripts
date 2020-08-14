7350ace42632a1a58cb32318c135f0e1ce2c1d57
Evan Leahy <49353272+echolimauw@users.noreply.github.com>
2020-08-14 14:55:31 -0700

    Updated Mover
    Mover updated to connect to animation controller.

--- Scripts/Mover.cs
+++ Scripts/Mover.cs
@@ -14,6 +14,8 @@ public class Mover : MonoBehaviour
         {
             MoveToCursor();
         }
+
+        UpdateAnimator();
     }
 
     private void MoveToCursor()
@@ -26,5 +28,13 @@ public class Mover : MonoBehaviour
         {
             GetComponent<NavMeshAgent>().destination = hit.point;
         }
+    }
+
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("forwardSpeed", speed);
     }
 }
