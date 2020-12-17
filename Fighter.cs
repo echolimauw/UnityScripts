dceb55cec50d51a2b045b58a5b88a34d1c3740ec
Evan Leahy <49353272+echolimauw@users.noreply.github.com>
2020-12-16 21:04:23 -0800

    Fixed dependency error
    missing dependency to allow Fighter.cs to access Mover.cs

---
+++ Fighter.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using RPG2.Movement;
+using RPG2.Core;
+
+namespace RPG2.Combat
+{
+    public class Fighter : MonoBehaviour, IAction
+    {
+        [SerializeField] float weaponRange = 2f;
+
+        Transform target;
+
+        private void Update()
+        {
+            if (target == null) return;
+
+            if (target != null && !GetIsInRange())
+            {
+                GetComponent<Mover>().MoveTo(target.position);
+            }
+            else
+            {
+                GetComponent<Mover>().Cancel();
+            }
+        }
+
+        private bool GetIsInRange()
+        {
+            return Vector3.Distance(transform.position, target.position) < weaponRange;
+        }
+
+        public void Attack(CombatTarget combatTarget)
+        {
+            GetComponent<ActionScheduler>().StartAction(this);
+            target = combatTarget.transform;
+            print("Take that!");
+        }
+
+        public void Cancel()
+        {
+            target = null;
+        }
+
+    }
+}
